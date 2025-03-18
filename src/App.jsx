4c9b7e904a094dedc0d466b2b064052614f71b5f
xKaktus <greinerlukasz@gmail.com>
2025-03-18 20:10:19 +0100

    Removed about page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import QuizesPage from "./pages/QuizesPage";
 import AddQuizForm from "./components/AddQuizForm";
-import About from "./pages/About";
 import NotFoundPage from "./pages/NotFoundPage";
 import Layout from "./components/Layout";
 
@@ -17,7 +16,6 @@ function App() {
           <Route exact path="/" component={HomePage} />
           <Route path="/quizes" component={QuizesPage} />
           <Route path="/add-quiz" component={AddQuizForm} />
-          <Route path="/about" component={About} />
           <Route component={NotFoundPage} />
         </Switch>
       </Layout>
