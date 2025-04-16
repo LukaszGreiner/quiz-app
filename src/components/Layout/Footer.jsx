0ef141dbec9a785840d761d354d711264929ca8d
xKaktus <greinerlukasz@gmail.com>
2025-04-16 13:26:36 +0200

    Logo font changed, footer content changed

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-primary mt-auto p-4 text-white">
       <div className="container mx-auto text-center">
-        <p>&copy; 2025 My App. All rights reserved.</p>
+        <p>&copy; 2025 Quizowisko</p>
       </div>
     </footer>
   );
--- src/components/Layout/Logo.jsx
+++ src/components/Layout/Logo.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router";
 export default function Logo() {
   return (
     <Link to="/">
-      <span className="font-poppins text-2xl font-bold text-nowrap">
-        My App
+      <span className="font-ptserif text-2xl font-bold text-nowrap">
+        Quizowisko
       </span>
     </Link>
   );
